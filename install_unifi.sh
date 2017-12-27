50b29944eae48610e3056868c60a80db98292178
Aryo <aryonp@gmail.com>
2017-12-27 21:00:23 +0700

    additional solution

--- install_unifi.sh
+++ install_unifi.sh
@@ -14,7 +14,8 @@ echo "** Install main software"
 {
 	sudo apt install -y unifi
 	sudo wget -O /tmp/unifi-video.deb https://dl.ubnt.com/firmwares/ufv/v3.8.5/unifi-video.Debian7_amd64.v3.8.5.deb
-	sudo apt install /tmp/unifi-video.deb 
+	sudo dpkg -i /tmp/unifi-video.deb
+	sudo apt install -f
 } &> /dev/null
 
 echo "** Install supporting software"
