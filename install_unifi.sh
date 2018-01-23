adb87e6ca0fdca992ebac65df017ff7aa8f50e5d
aryonp <1676948+aryonp@users.noreply.github.com>
2018-01-24 04:19:12 +0700

    Updates

--- install_unifi.sh
+++ install_unifi.sh
@@ -3,8 +3,9 @@ clear
 echo "** Starting installation"
 echo "** Add & update needed repositories"
 {
-	echo 'deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
-	sudo wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ubnt.com/unifi/unifi-repo.gpg 
+	echo "deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti" | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
+	echo "deb http://dl.ubnt.com/unifi-voip/stage/debian beta ubiquiti" | sudo tee /etc/apt/sources.list.d/110-ubnt-unifi-voip.list
+	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 52F815F3
 	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50 
 	sudo apt-add-repository -y ppa:webupd8team/java
 	sudo apt update 
@@ -12,12 +13,9 @@ echo "** Add & update needed repositories"
 
 echo "** Install main software"
 {
-	sudo apt install -y unifi
+	sudo apt install -y unifi unifi-voip
 	sudo wget -O /tmp/unifi-video.deb https://dl.ubnt.com/firmwares/ufv/v3.9.0/unifi-video.Ubuntu16.04_amd64.v3.9.0.debU
 	sudo dpkg -i /tmp/unifi-video.deb
-	sudo apt install -f
-	sudo wget -O /tmp/unifi-voip.deb https://dl.ubnt.com/unifi-voip/1.0.5-kxe7d9/unifi_voip_sysvinit_all.deb
-	sudo dpkg -i /tmp/unifi-voip.deb
 	sudo apt install -f
 } &> /dev/null
 
@@ -28,7 +26,7 @@ echo "** Restart all services"
 {
 	sudo service unifi restart
 	sudo service unifi-video restart
-	sudo service unifi-voip
+	sudo service unifi-voip restart
 } &> /dev/null
 
 echo "** Installation done"
