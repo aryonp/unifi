721459d0b564d2e714571f11d09d25a6f7a23fd7
aryonp <1676948+aryonp@users.noreply.github.com>
2018-01-29 08:05:02 +0700

    simplification

--- install_unifi_odroid_xu4.sh
+++ install_unifi_odroid_xu4.sh
@@ -3,8 +3,7 @@ clear
 echo "** Starting installation"
 echo "** Add & update needed repositories"
 {
-	echo 'deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
-	sudo wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ubnt.com/unifi/unifi-repo.gpg 
+	echo "deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti" | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
 	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50 
 	sudo apt-add-repository -y ppa:webupd8team/java
 	sudo apt update 
