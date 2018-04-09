16a1a5e72f8924e667351e1fd5e3103800ccf01c
aryonp <1676948+aryonp@users.noreply.github.com>
2018-04-09 15:24:21 +0700

    add log rotation

--- install_unifi_odroid_xu4.sh
+++ install_unifi_odroid_xu4.sh
@@ -21,4 +21,17 @@ sudo systemctl start unifi
 sudo systemctl disable mongodb
 sudo systemctl stop mongodb
 
+echo "** Install Log Rotation"
+sudo bash -c 'cat >> /etc/logrotate.d/unifi << EOF
+/var/log/unifi/*.log {
+    rotate 5
+    daily
+    missingok
+    notifempty
+    compress
+    delaycompress
+    copytruncate
+}
+EOF'
+
 echo "** Installation done"
