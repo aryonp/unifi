ab8999f008f19186b2071669a6a3bcbcb891bb54
aryonp <1676948+aryonp@users.noreply.github.com>
2018-04-09 15:03:40 +0700

    update features

--- install_unifi_odroid_xu4.sh
+++ install_unifi_odroid_xu4.sh
@@ -13,9 +13,12 @@ echo "** Install main software"
 sudo apt install -y unifi &> /dev/null
 
 echo "** Install supporting software"
-sudo apt install -y software-properties-common oracle-java8-installer oracle-java8-set-default oracle-java8-unlimited-jce-policy &> /dev/null
+sudo apt install -y logrotate software-properties-common oracle-java8-installer oracle-java8-set-default oracle-java8-unlimited-jce-policy &> /dev/null
 
 echo "** Restart services"
-sudo service unifi restart &> /dev/null
+sudo systemctl enable unifi
+sudo systemctl start unifi
+sudo systemctl disable mongodb
+sudo systemctl stop mongodb
 
 echo "** Installation done"
