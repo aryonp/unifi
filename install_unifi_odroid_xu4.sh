90ed7224d2e764361bc910894bbd91ac6a6788f3
aryonp <1676948+aryonp@users.noreply.github.com>
2018-06-12 15:21:43 +0700

    Update install_unifi_odroid_xu4.sh

--- install_unifi_odroid_xu4.sh
+++ install_unifi_odroid_xu4.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 clear
 echo "** Starting installation"
+echo ""
 echo "** Add & update needed repositories"
-{
-	echo "deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti" | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
-	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50 
-	sudo apt-add-repository -y ppa:webupd8team/java
-	sudo apt update 
-} &> /dev/null
-
+echo "deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti" | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50 
+sudo apt-add-repository -y ppa:webupd8team/java
+sudo apt update 
+	
 echo "** Install main software"
-sudo apt install -y unifi &> /dev/null
+sudo apt install -y unifi
 
 echo "** Install supporting software"
-sudo apt install -y logrotate software-properties-common oracle-java8-installer oracle-java8-set-default oracle-java8-unlimited-jce-policy &> /dev/null
+sudo apt install -y logrotate software-properties-common oracle-java8-installer oracle-java8-set-default oracle-java8-unlimited-jce-policy
 
 echo "** Restart services"
 sudo systemctl enable unifi
