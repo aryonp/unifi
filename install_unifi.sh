1db105d4784d9ad18ab8499863bb8033cbff4f93
aryonp <aryonp@gmail.com>
2020-05-25 16:26:40 +0700

    Update install_unifi.sh

--- install_unifi.sh
+++ install_unifi.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/env bash
 clear
 
-v_video=v3.10.2
-v_voip=1.0.5-kxe7d9
+v_video=v3.10.12
+v_voip=1.0.6-vfy6nm
 
 echo "** Starting installation"
-echo "** Add & update needed repositories"
-echo 'deb http://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-sudo apt update
+echo "** Prepare Software Repositories"
+echo 'deb https://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
+wget -qO - https://www.mongodb.org/static/pgp/server-3.4.asc | sudo apt-key add -
+echo "deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ui.com/unifi/unifi-repo.gpg 
+sudo apt-get update
 
 echo "** Install supporting software"
-sudo apt install -y apt-transport-https logrotate software-properties-common ca-certificates-java binutils jsvc libcommons-daemon-java openjdk-8-jre-headless mongodb-server
+sudo apt update && sudo apt install -y ca-certificates apt-transport-https logrotate software-properties-common ca-certificates-java binutils jsvc libcommons-daemon-java openjdk-8-jre-headless mongodb-server
+
 
 echo "** Install main software"
 sudo apt install -y unifi 
 sudo wget -O /tmp/unifi-video.deb "https://dl.ubnt.com/firmwares/ufv/${v_video}/unifi-video.Ubuntu18.04_amd64.${v_video}.deb"
 sudo wget -O /tmp/unifi-voip.deb "https://dl.ubnt.com/unifi-voip/${v_voip}/unifi_voip_sysvinit_all.deb"
-sudo dpkg -i /tmp/unifi-video.deb
-sudo dpkg -i /tmp/unifi_voip.deb
-sudo apt install -f
+sudo gdebi /tmp/unifi-video.deb
+sudo gdebi /tmp/unifi_voip.deb
 
 echo "** Restart all services"
 sudo systemctl enable unifi
