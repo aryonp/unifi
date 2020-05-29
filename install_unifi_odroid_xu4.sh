820b9fc3407459b36a4893a1999449a50a290f94
aryonp <aryonp@gmail.com>
2020-05-30 00:26:22 +0700

    Add MongoDB 3.2

--- install_unifi_odroid_xu4.sh
+++ install_unifi_odroid_xu4.sh
@@ -4,12 +4,18 @@ v_unifi=5.12.72
 
 echo "** Starting installation"
 echo ""
-echo "** Add & update needed repositories"
-#repo mongodb
-#on fix
+echo "** Download MongoDB 3.2 armhf -- Thx Dominic Chen!"
+wget https://github.com/ddcc/mongodb/releases/download/v3.2.22-2/mongodb-clients_3.2.22-2_armhf.deb
+wget https://github.com/ddcc/mongodb/releases/download/v3.2.22-2/mongodb-server-core_3.2.22-2_armhf.deb
+wget https://github.com/ddcc/mongodb/releases/download/v3.2.22-2/mongodb-server_3.2.22-2_all.deb
+wget https://github.com/ddcc/mongodb/releases/download/v3.2.22-2/mongodb_3.2.22-2_armhf.deb
 	
 echo "** Install supporting software"
 sudo apt install -y apt-transport-https logrotate software-properties-common ca-certificates-java binutils jsvc libcommons-daemon-java openjdk-8-jre-headless debsums gdebi
+sudo gdebi mongodb-clients_3.2.22-2_armhf.deb
+sudo gdebi mongodb-server-core_3.2.22-2_armhf.deb
+sudo gdebi mongodb-server_3.2.22-2_all.deb
+sudo gdebi mongodb_3.2.22-2_armhf.deb
 
 echo "** Install main software"
 wget https://dl.ubnt.com/unifi/${v_unifi}/unifi_sysvinit_all.deb
