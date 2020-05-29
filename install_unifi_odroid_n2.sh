32d305c7b4b25db9cdfe44f6a18e34d6afc18330
aryonp <aryonp@gmail.com>
2020-05-30 00:11:07 +0700

    Update install_unifi_odroid_n2.sh

--- install_unifi_odroid_n2.sh
+++ install_unifi_odroid_n2.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 clear
 v_unifi=5.12.72
-
 echo "** Starting installation"
 echo "" 
-echo "** Install supporting software"
+echo "** Update everything first"
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y jsvc logrotate openjdk-8-jre-headless ufw libcap2 ca-certificates apt-transport-https gdebi debsums
+echo "" 
+echo "** Add needed repositories"
 wget -qO - https://www.mongodb.org/static/pgp/server-3.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-sudo apt update && sudo apt install -y mongodb-org
-
+echo "" 
+echo "** Install supporting software"
+sudo apt update && sudo apt install -y jsvc logrotate openjdk-8-jre-headless ufw libcap2 ca-certificates apt-transport-https gdebi debsums mongodb-org
+echo "" 
 echo "** Install main software"
 wget https://dl.ubnt.com/unifi/${v_unifi}/unifi_sysvinit_all.deb
 sudo gdebi unifi_sysvinit_all.deb
-
+echo "" 
 echo "** Restart services"
 sudo systemctl enable unifi
 sudo systemctl start unifi
-
+echo "" 
 echo "** Hold Update for JVSC, Unifi, MongoDB"
 sudo apt-mark hold jsvc
 sudo apt-mark hold unifi
 sudo apt-mark hold mongodb-org
-
+echo "" 
 echo "** Install Log Rotation"
 sudo bash -c 'cat >> /etc/logrotate.d/unifi << EOF
 /var/log/unifi/*.log {
@@ -36,5 +38,5 @@ sudo bash -c 'cat >> /etc/logrotate.d/unifi << EOF
     copytruncate
 }
 EOF'
-
+echo "" 
 echo "** Installation done"
