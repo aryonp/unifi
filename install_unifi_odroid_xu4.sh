e1fc7b10c867f09366c0ebe0e5aa3028330a7842
aryonp <aryonp@gmail.com>
2020-07-05 12:09:12 +0000

    Update install_unifi_odroid_xu4.sh

--- install_unifi_odroid_xu4.sh
+++ install_unifi_odroid_xu4.sh
@@ -5,21 +5,22 @@ v_unifi=5.13.32
 echo "** Starting installation"
 echo ""
 echo "** Download MongoDB 3.2 armhf -- Thx Dominic Chen!"
-wget https://github.com/ddcc/mongodb/releases/download/v3.2.22-2/mongodb-clients_3.2.22-2_armhf.deb
-wget https://github.com/ddcc/mongodb/releases/download/v3.2.22-2/mongodb-server-core_3.2.22-2_armhf.deb
-wget https://github.com/ddcc/mongodb/releases/download/v3.2.22-2/mongodb-server_3.2.22-2_all.deb
-wget https://github.com/ddcc/mongodb/releases/download/v3.2.22-2/mongodb_3.2.22-2_armhf.deb
+wget -c https://github.com/ddcc/mongodb/releases/download/v3.2.22-2/mongodb-clients_3.2.22-2_armhf.deb
+wget -c https://github.com/ddcc/mongodb/releases/download/v3.2.22-2/mongodb-server-core_3.2.22-2_armhf.deb
+wget -c https://github.com/ddcc/mongodb/releases/download/v3.2.22-2/mongodb-server_3.2.22-2_all.deb
+wget -c https://github.com/ddcc/mongodb/releases/download/v3.2.22-2/mongodb_3.2.22-2_armhf.deb
 	
 echo "** Install supporting software"
-sudo apt install -y apt-transport-https logrotate software-properties-common ca-certificates-java binutils jsvc libcommons-daemon-java openjdk-8-jdk debsums gdebi curl wget speedtest-cli ufw
-sudo gdebi mongodb-clients_3.2.22-2_armhf.deb
-sudo gdebi mongodb-server-core_3.2.22-2_armhf.deb
-sudo gdebi mongodb-server_3.2.22-2_all.deb
-sudo gdebi mongodb_3.2.22-2_armhf.deb
+sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade && sudo apt install -y apt-transport-https logrotate software-properties-common ca-certificates-java binutils jsvc libcommons-daemon-java openjdk-8-jdk debsums gdebi curl wget speedtest-cli ufw
+sudo dpkg -i  mongodb-clients_3.2.22-2_armhf.deb
+sudo apt-get install --fix-broken
+sudo dpkg -i mongodb-server-core_3.2.22-2_armhf.deb
+sudo dpkg -i mongodb-server_3.2.22-2_all.deb
+sudo dpkg -i mongodb_3.2.22-2_armhf.deb
 
 echo "** Install main software"
-wget https://dl.ubnt.com/unifi/${v_unifi}/unifi_sysvinit_all.deb
-sudo gdebi unifi_sysvinit_all.deb
+wget -c https://dl.ubnt.com/unifi/${v_unifi}/unifi_sysvinit_all.deb
+sudo dpkg -i unifi_sysvinit_all.deb
 
 echo "** Open UFW Ports"
 sudo ufw allow 3478/udp
@@ -34,6 +35,8 @@ sudo ufw allow 5656:5699/udp
 sudo ufw allow 10001/udp
 sudo ufw allow 1900/udp
 sudo ufw allow 443
+sudo ufw allow 22
+sudo ufw allow 80
 sudo ufw allow 8883/tcp
 
 echo "** Restart services"
