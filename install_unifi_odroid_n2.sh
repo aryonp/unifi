b978913f05a2c6129fb450f336730970c8954c6b
aryonp <aryonp@gmail.com>
2020-07-20 12:17:36 +0700

    Update install_unifi_odroid_n2.sh

--- install_unifi_odroid_n2.sh
+++ install_unifi_odroid_n2.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 clear
-v_unifi=5.13.32
 echo "** Starting installation"
-echo "" 
-echo "** Update everything first"
-sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade
-echo "" 
+
 echo "** Add needed repositories"
 wget -qO - https://www.mongodb.org/static/pgp/server-3.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-echo "" 
-echo "** Install supporting software"
-sudo apt update && sudo apt install -y jsvc logrotate openjdk-8-jre-headless ufw libcap2 ca-certificates apt-transport-https gdebi debsums mongodb-org wget curl ufw
-echo "" 
-echo "** Install main software"
-wget https://dl.ubnt.com/unifi/${v_unifi}/unifi_sysvinit_all.deb
-sudo gdebi unifi_sysvinit_all.deb
-echo "" 
+
+echo 'deb https://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
+sudo wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ui.com/unifi/unifi-repo.gpg 
+
+echo "** Update everything first"
+sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade
+
+echo "** Install supporting software & unifi controller"
+sudo apt install -y jsvc logrotate openjdk-8-jre-headless ufw libcap2 ca-certificates apt-transport-https gdebi debsums mongodb-org wget curl ufw unifi
+
 echo "** Restart services"
 sudo systemctl enable unifi
 sudo systemctl start unifi
-echo "" 
+
 echo "** Open UFW Ports"
 sudo ufw allow 3478/udp
 sudo ufw allow 5514/tcp	
@@ -37,12 +35,7 @@ sudo ufw allow 443
 sudo ufw allow 22
 sudo ufw allow 80
 sudo ufw allow 8883/tcp
-echo "" 
-echo "** Hold Update for JVSC, Unifi, MongoDB"
-sudo apt-mark hold jsvc
-sudo apt-mark hold unifi
-sudo apt-mark hold mongodb-org
-echo "" 
+
 echo "** Install Log Rotation"
 sudo bash -c 'cat >> /etc/logrotate.d/unifi << EOF
 /var/log/unifi/*.log {
@@ -55,5 +48,5 @@ sudo bash -c 'cat >> /etc/logrotate.d/unifi << EOF
     copytruncate
 }
 EOF'
-echo "" 
+
 echo "** Installation done"
